5b5d80eb1f7a913e06167d59b55db89225fa61cb
Muhammad Adil <106780096+Mian-Addi2500@users.noreply.github.com>
2023-09-22 08:41:44 +0000

    varible file added

---
+++ 01_Basics/01_varibles.js
@@ -0,0 +1,11 @@
+const accountId = 123454433
+let accountName = "Mian"
+var accountPasward = "11221122"
+accountCity = "Mailsi"
+
+let accountState ;
+console.log(accountId);
+console.table([accountId,accountName,accountPasward, accountCity , accountState])
+//accountId = 12 // constant varibales not changed 
+accountName = "addi"
+console.table([accountId,accountName,accountPasward, accountCity , accountState])
